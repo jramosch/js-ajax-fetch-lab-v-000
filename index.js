ea07cba373fa52ea3c436e99f3ed440ec2611791
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-05-31 15:28:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ function createIssue() {
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
   fetch(`${baseURL}/repos/${user_repo}/issues`, {
     headers: {
       Authorization: `token ${getToken()}`
